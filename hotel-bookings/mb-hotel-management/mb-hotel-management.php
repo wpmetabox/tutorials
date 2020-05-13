8bf3f4ccee107c00cdc4c74587c3a9ad6d53dfc1
Tran Ngoc Tuan Anh <anhtnt@elightup.com>
2020-05-13 15:29:11 +0700

    Update mb-hotel-management.php

--- hotel-bookings/mb-hotel-management/mb-hotel-management.php
+++ hotel-bookings/mb-hotel-management/mb-hotel-management.php
@@ -53,30 +53,32 @@ class managerBooking {
 
 		$query = new WP_Query( $args );
 
-		if ( $query->have_posts() ) {
-			$events = array();
-			while( $query->have_posts() ) {  $query->the_post();
-				$bookings = get_post_meta( get_the_ID(), 'group_booking', true );
-				if ($bookings) {
-					foreach ($bookings as $key => $booking) {
-						$room = $booking['room'];
-						$begin = $booking['check_in']; 
-						$end = $booking['check_out']; 
-
-						$e['title'] = "#".get_the_ID().' '.get_the_title($room);
-						$e['start'] = $begin;
-						$e['end'] =  $end;
-						$e['url'] =  get_edit_post_link(get_the_ID(),'');
-						$e['classNames'] = 'room-'.$room; 
-						$e['allDay'] = true;
-
-						array_push($events, $e);
-					}
-				}
-			}
-
-			echo json_encode($events);
+		if ( ! $query->have_posts() ) {
+			wp_die();
 		}
+		$events = array();
+		while( $query->have_posts() ) {  $query->the_post();
+			$bookings = get_post_meta( get_the_ID(), 'group_booking', true );
+			if ( empty( $bookings ) ) {
+				continue;
+			}
+			foreach ($bookings as $key => $booking) {
+				$room = $booking['room'];
+				$begin = $booking['check_in']; 
+				$end = $booking['check_out']; 
+
+				$e['title'] = "#".get_the_ID().' '.get_the_title($room);
+				$e['start'] = $begin;
+				$e['end'] =  $end;
+				$e['url'] =  get_edit_post_link(get_the_ID(),'');
+				$e['classNames'] = 'room-'.$room; 
+				$e['allDay'] = true;
+
+				array_push($events, $e);
+			}
+		}
+
+		echo json_encode($events);
 		wp_die();
 	}
 }
