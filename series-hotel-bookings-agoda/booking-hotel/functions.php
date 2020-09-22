0c09c7e143dd7bd43c02649cbbbb645e85f6d4d5
longvt93 <vutuanlongdhcn@gmail.com>
2020-09-22 23:59:42 +0700

    Fix child theme style.css

--- series-hotel-bookings-agoda/booking-hotel/functions.php
+++ series-hotel-bookings-agoda/booking-hotel/functions.php
@@ -13,13 +13,13 @@
  * Enqueue scripts and styles.
  */
 function justread_child_scripts() {
-	$parenthandle = 'justread';
+	$parenthandle = 'justread-style';
 	$theme = wp_get_theme();
 	wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
 		array(),
 		$theme->parent()->get('Version')
 	);
-	// wp_enqueue_style( 'justread-child-style', get_stylesheet_uri(), array(), '1.0.0' );
+	wp_enqueue_style( 'justread-child-style', get_stylesheet_directory_uri() . '/style.css' );
 
 	if ( is_singular( 'hotel' ) ) {
 		wp_enqueue_script( 'justread-slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array(), '1.8.1', true );
