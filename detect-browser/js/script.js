f62301e6c9c63741ada3694564c20a4c988c35bc
longvt93 <vutuanlongdhcn@gmail.com>
2021-03-06 00:32:33 +0700

    Update detect-browser

--- detect-browser/js/script.js
+++ detect-browser/js/script.js
@@ -3,29 +3,35 @@ import { createHooks } from '@wordpress/hooks';
 jQuery( function ($) {
 
 	let globalHooks = createHooks();
-	function detect_browser() {
+	function getBrowser() {
+		let result = '';
 		if ( ( navigator.userAgent.indexOf( 'Opera' ) || navigator.userAgent.indexOf( 'OPR' ) ) != -1 ) {
-			console.log( 'Opera Browser' );
+			result = 'Opera Browser';
 		}
 		else if ( navigator.userAgent.indexOf( 'Chrome' ) != -1 ) {
-			console.log( 'Hi Chrome Browser' );
+			result = 'Hi Chrome Browser';
 		}
 		else if ( navigator.userAgent.indexOf( 'Safari' ) != -1 ) {
-			console.log( 'Safari Browser' );
+			result = 'Safari Browser';
 		}
 		else if ( navigator.userAgent.indexOf( 'Firefox' ) != -1 )  {
-			console.log( 'Firefox Browser' );
+			result = 'Firefox Browser';
 		}
 		else if ( ( navigator.userAgent.indexOf( 'MSIE' ) != -1 ) || ( !! document.documentMode == true ) ) {
-			console.log( 'IE Browser' ); 
+			result = 'IE Browser';
 		}
 		else {
-			console.log( 'unknown Browser' );
+			result = 'unknown Browser';
 		}
-
+		return globalHooks.applyFilters( 'detect_browser', result );
 	}
-	// detect_browser();
-	globalHooks.applyFilters( 'detect_browser', detect_browser() );
 
-	// globalHooks.removeAllFilters( 'detect_browser' );
+	
+	globalHooks.addFilter( 'detect_browser', 'myApp', filterGetBrowser );
+	function filterGetBrowser() {
+		return navigator.appVersion;
+	}
+
+
+	console.log( getBrowser() );
 } );
