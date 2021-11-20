0996ef4af4eaadf02b4e38b53a40fe52004560a8
Tran Manh Cuong <70245774+imcuong98@users.noreply.github.com>
2021-11-20 14:24:29 +0700

    Update custom.js

--- display-latest-posts-with-MB-Views/my-custom-functionality-master/assets/js/custom.js
+++ display-latest-posts-with-MB-Views/my-custom-functionality-master/assets/js/custom.js
@@ -1,18 +1,5 @@
 jQuery(document).ready(function ($) {
-    $(".slider-product").slick({
-        infinite: false,
-        // arrows: false,
-        dots: false,
-        autoplay: false,
-        speed: 800,
-        autoplaySpeed: 4000,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        nextArrow: '<button class="next-btn">Next</button>',
-        prevArrow: '<button class="prev-btn">Previous</button>'
-    });
-
-    $(".list-restaurant .slider-res").slick({
+    jQuery(".list-restaurant .slider-res").slick({
         infinite: false,
         dots: false,
         autoplay: false,
@@ -22,22 +9,13 @@ jQuery(document).ready(function ($) {
         slidesToScroll: 3,
         nextArrow: '<button class="next-btn">Next</button>',
         prevArrow: '<button class="prev-btn">Previous</button>'
-    });
-
-    jQuery('.discount-price span').each(function () {
-        if (jQuery(this).not(':empty').length) {
-            jQuery(this).parent().addClass('no');
-        } else {
-            jQuery(this).parent().hide();
-            jQuery(this).parent().next().addClass('full-width');
-        }
     });
 
     jQuery('.status-res span').each(function () {
-        if (jQuery(this).html() == 'online') {
-            jQuery(this).parent().addClass('online');
+        if (jQuery(this).html() == 'open') {
+            jQuery(this).parent().addClass('open');
         } else {
-            jQuery(this).parent().addClass('offline');
+            jQuery(this).parent().addClass('close');
         }
     });
 
