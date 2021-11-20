faac01422c3c2dbad3670ad4b8e5044e8f3a07ed
Tran Manh Cuong <70245774+imcuong98@users.noreply.github.com>
2021-11-20 14:14:53 +0700

    Update custom.js

--- display-latest-posts-with-Oxygen/my-custom-functionality-master/assets/js/custom.js
+++ display-latest-posts-with-Oxygen/my-custom-functionality-master/assets/js/custom.js
@@ -1,16 +1,5 @@
 jQuery(document).ready(function ($) {
-    $(".best-selling-product ul").slick({
-        infinite: false,
-        dots: false,
-        autoplay: false,
-        speed: 800,
-        autoplaySpeed: 4000,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        nextArrow: '<button class="next-btn">Next</button>',
-        prevArrow: '<button class="prev-btn">Previous</button>'
-    });
-    $(".list-restaurant .slider-res").slick({
+    jQuery(".list-restaurant .slider-res").slick({
         infinite: false,
         dots: false,
         autoplay: false,
