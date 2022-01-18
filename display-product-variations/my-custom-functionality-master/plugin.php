cf49f5935627b41d6afa814de769ab3b3374a352
Tran Manh Cuong <70245774+imcuong98@users.noreply.github.com>
2022-01-18 11:55:42 +0700

    How to display Product Variations

---
+++ display-product-variations/my-custom-functionality-master/plugin.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Plugin Name:	My Custom Functionality
+Plugin URI:		https://example.com
+Description:	My custom functions.
+Version:		1.0.0
+Author:			Your Name
+Author URI:		https://example.com
+License:		GPL-2.0+
+License URI:	http://www.gnu.org/licenses/gpl-2.0.txt
+
+This plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+This plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with This plugin. If not, see {URI to Plugin License}.
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_action( 'wp_enqueue_scripts', 'custom_enqueue_files' );
+/**
+ * Loads <list assets here>.
+ */
+function custom_enqueue_files() {
+	// if this is not the front page, abort.
+	// if ( ! is_front_page() ) {
+	// 	return;
+	// }
+
+	// loads a CSS file in the head.
+	// wp_enqueue_style( 'highlightjs-css', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
+
+	/**
+	 * loads JS files in the footer.
+	 */
+	// wp_enqueue_script( 'highlightjs', plugin_dir_url( __FILE__ ) . 'assets/js/highlight.pack.js', '', '9.9.0', true );
+
+	// wp_enqueue_script( 'highlightjs-init', plugin_dir_url( __FILE__ ) . 'assets/js/highlight-init.js', '', '1.0.0', true );
+	wp_enqueue_style('slick', plugin_dir_url( __FILE__ ).'/assets/css/slick.css');
+	wp_enqueue_style('slick-theme', plugin_dir_url( __FILE__ ).'/assets/css/slick-theme.css');
+	wp_enqueue_style('custom-css', plugin_dir_url( __FILE__ ).'/assets/css/custom.css');
+	wp_enqueue_style('css-css', plugin_dir_url( __FILE__ ).'/assets/css/css.css');
+
+	wp_enqueue_script('js-js', plugin_dir_url( __FILE__ ).'/assets/js/js.js', ['jquery']);
+	wp_enqueue_script('custom', plugin_dir_url( __FILE__ ).'/assets/js/custom.js', ['jquery']);
+	wp_enqueue_script('slick-min', plugin_dir_url( __FILE__ ).'/assets/js/slick.min.js', ['jquery']);
+}
---
+++ display-product-variations/template.php
@@ -0,0 +1,128 @@
+{% set product_types = post.does_this_product_have_variations %}
+<div class="container detail-product">
+	{% if (product_types  == 0) %}
+	 	<div class="page-layout simple-product">
+			<div class="gallery-side">
+					<div class="group-image">
+								<div class="slider slider-single">
+									{% for item in post.simple_product.product_images %}
+										<div class="img-slider">
+											<img src="{{ item.large.url }}"/>
+										</div>	
+									{% endfor %}
+								</div>
+								<div class="slider slider-nav">
+									{% for item in post.simple_product.product_images %}
+									 <div class="gallery-slide">
+											<img src="{{ item.thumbnail.url }}"/>
+									</div>
+									{% endfor %}
+								</div>
+					</div>
+			</div>
+			<div class="info-detail-product">
+				<h1 class="title-product">{{ post.title }}</h1>
+				<h2 class="description-product">{{ post.content }}</h2>
+				<div class="price-contain-group">
+					<div class="price-group">
+						<div class="price">
+							{% if post.simple_product.promotional_price|trim is empty %} 
+							<div class="original-price">{{ post.simple_product.original_price }}</div>
+							{% else %}
+							<div class="original-price promotional-price">{{ post.simple_product.promotional_price }}</div>
+							<div class="old-price">{{ post.simple_product.original_price }}</div>
+							{% endif %}
+						</div>
+					</div>
+				</div>
+				<div class="size-contain-group">
+					<p class="type-size">Size:</p>
+					<div class="size-group">
+						<div class="info">
+							<div class="list-size">
+							    {% for item in post.simple_product.size %}
+								<p class="size-name {% if loop.first %}active{% endif %}">{{ item.label }}</p>
+								{% endfor %}
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="status">{{ post.simple_product.status.label }}</div>
+				<button id="{{ post.ID }}" class="order-now">Order Now</button>
+			</div>
+		</div>
+		 {% else %}
+		 	<div class="page-layout grouped-product">
+				<div class="gallery-side">
+					{% for clone in post.variations_of_product %}
+					<div class="group-image {% if loop.first %}active{% endif %}" data-id="{{ clone.color_name.value }}">
+						<div class="slider slider-single">
+							{% for item in clone.product_images %}
+							<div class="img-slider">
+								<img src="{{ item.large.url }}"/>
+							</div>
+							{% endfor %}
+						</div>
+						<div class="slider slider-nav">
+							{% for item in clone.product_images %}
+							<div class="gallery-slide">
+								<img src="{{ item.thumbnail.url }}"/>
+							</div>
+							{% endfor %}
+						</div>
+					</div>
+					{% endfor %}
+				</div>
+				<div class="info-detail-product">
+					<h1 class="title-product">{{ post.title }}</h1>
+					<h2 class="description-product">{{ post.content }}</h2>
+					<div class="color-contain-group">
+						<p class="type-color">Color:</p>
+							<div class="color-group">
+								{% for clone in post.variations_of_product %}
+									<div class="color-name {% if loop.first %}active{% endif %}">
+										<a href="#{{ clone.color_name.value }}" class="color {{ clone.color_name.value }}"  title="{{ clone.color_name.label }}">{{ clone.color_name.label }}</a>
+									</div>
+								{% endfor %}
+							</div>	
+					</div>
+					<div class="price-contain-group">
+						{% for clone in post.variations_of_product %}
+						<div class="price-group {% if loop.first %}active{% endif %}" data-id="{{ clone.color_name.value }}">
+							<div class="price">
+							{% if clone.promotional_price|trim is empty %} 
+								<div class="original-price">{{ clone.original_price }}</div>
+								{% else %}
+								<div class="promotional-price">{{ clone.promotional_price }}</div>
+								<div class="original-price">{{ clone.original_price }}</div>
+							{% endif %}
+							</div>
+						</div>
+						{% endfor %}
+						</div>
+						<div class="size-contain-group">
+							<p class="type-size">Size:</p>
+							<div class="size-group">
+								{% for clone in post.variations_of_product %}
+								<div class="info {% if loop.first %}active{% endif %}" data-id="{{ clone.color_name.value }}">
+									<div class="list-size">
+										{% for item in clone.size %}
+										<p class="size-name {% if loop.first %}active{% endif %}">{{ item.label }}</p>
+										{% endfor %}
+									</div>
+								</div>
+								{% endfor %}
+							</div>
+						</div>
+						<div class="status-group">
+							{% for clone in post.variations_of_product %}
+							<div class="status {% if loop.first %}active{% endif %}" data-id="{{ clone.color_name.value }}">
+								{{ clone.status.label }}
+							</div>
+							{% endfor %}
+						</div>
+						<a class="open-button order-now" href="javascript:void(0)"> Order Now</a>
+				</div>
+			</div>
+			{% endif %}
+</div>
