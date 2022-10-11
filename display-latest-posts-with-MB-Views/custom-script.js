c99bc288fb35f283c0053468a8d4b9911ced37ba
Tran Manh Cuong <70245774+imcuong98@users.noreply.github.com>
2022-10-11 14:30:41 +0700

    Display latest posts with MB Views

---
+++ display-latest-posts-with-MB-Views/custom-script.js
@@ -0,0 +1,21 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
