6d990b836b500c1dc09624382146c888d397aa88
Tran Manh Cuong <70245774+imcuong98@users.noreply.github.com>
2022-11-16 16:01:01 +0700

    Update template.php

--- display-latest-posts-with-MB-Views/template.php
+++ display-latest-posts-with-MB-Views/template.php
@@ -1,24 +1,31 @@
-<div class="list-restaurant">
-	<div class="slider-res">
-{% set args = { post_type: 'restaurant', posts_per_page: -1, orderby: 'date', order: 'DESC' } %}
-{% set posts = mb.get_posts( args ) %}
-{% for post in posts %}
-	<div class="contain-restaurant">
-		<div class="cover-image-res">
-			<img src="{{ post.logo.thumbnail.url }}" width="{{ post.logo.thumbnail.width }}" height="{{ post.logo.thumbnail.height }}" alt="{{ post.logo.thumbnail.alt }}">
-			<div class="voucher-res">
-				<span>{% for item in post.voucher %}
-	{{ item.label }}
-{% endfor %}
-</span>
+<div class="list-restaurant slider">
+	<div class="slider-res slider__wrapper">
+		{% set args = { post_type: 'restaurant', posts_per_page: -1, orderby: 'date', order: 'DESC' } %}
+		{% set posts = mb.get_posts( args ) %}
+		{% for post in posts %}
+		<div class="contain-restaurant slider__item">
+			<div class="cover-image-res">
+				<img src="{{ post.thumbnail.large.url }}" width="{{ post.thumbnail.large.width }}" height="{{ post.thumbnail.large.height }}" alt="{{ post.thumbnail.large.alt }}">
+				<div class="voucher-res">
+					<span>
+					{% for item in post.voucher %}
+						{{ item.label }}
+					{% endfor %}
+					</span>
+				</div>
 			</div>
-			<div class="brand-res">
+			<div class="logo">
 				<img src="{{ post.logo.thumbnail.url }}" width="{{ post.logo.thumbnail.width }}" height="{{ post.logo.thumbnail.height }}" alt="{{ post.logo.thumbnail.alt }}">
-<a class="name-res" href="{{ mb.get_permalink( post.ID ) }}"> {{ post.post_title }}</a>
-		<div class="address-res"> {{ post.address }}</div>
-
-		<div class="status-res">
-			<span>{{ post.status.label }}</span>
+			</div>
+			<a class="name-res" href="{{ mb.get_permalink( post.ID ) }}"> {{ post.post_title }}</a>
+			<div class="address-res"> {{ post.address }}</div>
+			<div class="status-res">
+				<span>{{ post.status.label }}</span>
+			</div>
 		</div>
+		{% endfor %}
+    
+	</div>
+	<a class="slider__control slider__control_left" href="#" role="button"></a>
+    <a class="slider__control slider__control_right slider__control_show" href="#" role="button"></a>
 </div>
-<div>
