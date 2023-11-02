85a1c55d79cb69f49e7c3674c359935864e94e88
elumarketing <117496168+elumarketing@users.noreply.github.com>
2023-11-02 09:37:12 +0700

    Update function.php

--- displaying-images-as-gallery/function.php
+++ displaying-images-as-gallery/function.php
@@ -1,1 +1,13 @@
+function get_image_field() {
+    $images = rwmb_meta( 'image_gallery', ['size' => 'large'] );    
+    $html ='<div class="wrapper mb-slider">';
+        $html .='<div class="carousel">';
+            foreach ( $images as $image ) :
+                    $html .='<img src="'.$image['url'].'">';
+            endforeach ;
+        $html .='</div>';
+    $html .='</div>';
 
+    return $html;
+}
+add_shortcode( 'brand_image', 'get_image_field' );
