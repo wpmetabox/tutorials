4cffaf36f4f52c07f72519329ffb6a49dc96b35a
elumarketing <117496168+elumarketing@users.noreply.github.com>
2024-01-15 09:13:21 +0700

    Update header.php

--- create-a-product-page-width-divi/header.php
+++ create-a-product-page-width-divi/header.php
@@ -1,1 +1,2 @@
-
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="https://kenwheeler.github.io/slick/slick/slick-theme.css"/>
