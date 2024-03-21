5433bd17aab9b2c75632ce2899fc13b1631498b6
elumarketing <117496168+elumarketing@users.noreply.github.com>
2024-03-21 15:18:23 +0700

    Update script.js

--- video-playlist-with-mb-views/script.js
+++ video-playlist-with-mb-views/script.js
@@ -1,1 +1,10 @@
+jQuery(function ($) {
+    $("#video-playlist .video").on("click", function () {
+        $('.video').removeClass('active');
+        $(this).addClass('active');
+        $("#videoarea").attr({
+            "src": $(this).find('.video-name').attr("movieurl"),
+        })
+    })
+})
 
